019a268b78b45eb3c4e0b6ff58339004f5d3ecbc
romielisse <romielisse@gmail.com>
2018-10-24 14:21:16 -0400

    initial commit

---
+++ src/main/java/com/example/demo/TheNothingFormApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNothingFormApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TheNothingFormApplication.class, args);
+  }
+}
